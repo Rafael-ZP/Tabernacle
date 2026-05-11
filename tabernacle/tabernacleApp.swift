86feaa347178c9ce359f92e1759a42e1191ab61c
Rafael Zieganpalg <rafaelzieganpalg@gmail.com>
2026-05-11 13:12:29 +0530

    Initial Commit

---
+++ tabernacle/tabernacleApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabernacleApp.swift
+//  tabernacle
+//
+//  Created by Rafael Zieganpalg on 11/05/26.
+//
+
+import SwiftUI
+
+@main
+struct tabernacleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tabernacleTests/tabernacleTests.swift
@@ -0,0 +1,17 @@
+//
+//  tabernacleTests.swift
+//  tabernacleTests
+//
+//  Created by Rafael Zieganpalg on 11/05/26.
+//
+
+import Testing
+@testable import tabernacle
+
+struct tabernacleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
